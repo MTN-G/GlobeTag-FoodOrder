3b48b496c1085b9eb2e0fe45bfe66948c4324dd3
Matan Greenvlad <mgk2798@gmail.com>
2021-07-23 14:10:59 +0300

    append line endpoint

--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 const app = express();
 
@@ -8,28 +8,44 @@ app.use(express.json());
 const dirPath = "./files/";
 
 // create new file function
-const newFile = (date) => {
-  fs.writeFile(dirPath + date + ".txt", date, (err) => {
-    console.log(err);
-  });
+const newFile = async (fileName) => {
+  return fs
+    .writeFile(fileName, new Date().toDateString())
+    .then(() => {
+      return fileName;
+    })
+    .catch((err) => console.log(err.message));
 };
 
-const fileExist = () => {
-  let date = new Date();
-  date = date.toDateString();
-  if (fs.existsSync(dirPath + date + ".txt")) {
-  } else {
-    newFile(date);
+// checking if file exist. in case not, create it with newFile func.
+const fileExist = async () => {
+  let date = new Date().toDateString();
+  const fileName = dirPath + date + ".txt";
+  console.log(typeof fileName);
+  return fs
+    .access(fileName)
+    .then(() => {
+      return fileName;
+    })
+    .catch(async (err) => {
+      return await newFile(fileName);
+    });
+};
+
+// append new line function
+const newLine = async (file, data) => {
+  await fs.appendFile(file, "\n" + data + "\n-----------------------");
+};
+
+// main endpoint, recieve the client content and adding it to the daily text file
+app.post("/append", async (req, res) => {
+  const fileName = await fileExist();
+  try {
+    await newLine(fileName, req.body.newLine);
+    res.send("new line appended");
+  } catch (err) {
+    res.send(err);
   }
-};
-
-const newLine = (file, data) => {
-  fs.appendFile(file, "/n" + data + "/n-----------------------");
-};
-
-app.post("/append", (req, res) => {
-  fileExist();
-  res.send("ok");
 });
 
 module.exports = app;
